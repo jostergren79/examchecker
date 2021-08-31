88a5e1ab780f57df705481a8b253476f355e04cb
Jason Ostergren <jason@Jasons-MacBook-Pro.local>
2021-08-31 12:55:11 -0500

    updating files to align with project minimum requirements

--- public/js/add.js
+++ public/js/add.js
@@ -22,20 +22,37 @@ $("#add-btn").on("click", function(event) {
     authoritativeAnswer: $("#authoritativeAnswer").val().trim(),
 
     // result
-    result: $("#result").val().trim()
+    // result: $("#result").val().trim()
 
   };
 
+// This function validates the studentAnswer and authoritativeAnswer are parsed to the nearest integer and matched appropriately
   function validatedResult () {
   
-  let studentAnswerInt = parseInt(newInput.studentAnswer)
-  let authoritativeAnswerInt = parseInt(newInput.authoritativeAnswer)
+    let studentAnswerInt = parseInt(newInput.studentAnswer)
+    let authoritativeAnswerInt = parseInt(newInput.authoritativeAnswer)
+    let resultValue = (newInput.result)
+    
+    {
 
-  console.log(studentAnswerInt);
-  console.log(authoritativeAnswerInt); 
-  }
+        if (studentAnswerInt == authoritativeAnswerInt)
+        (resultValue = "Correct")
 
-validatedResult ();
+        else if (studentAnswerInt != authoritativeAnswerInt)
+        (resultValue = "Incorrect")
+
+        else if (studentAnswerInt != INTEGER)
+        (resultValue = "Invalid")
+
+    }
+  
+    console.log(studentAnswerInt);
+    console.log(authoritativeAnswerInt); 
+    console.log(resultValue);
+    alert((resultValue) + "Answer!");
+}
+
+  validatedResult ();
 
   // send an AJAX POST-request with jQuery
   $.post("/api/results/new", newInput)
@@ -44,7 +61,7 @@ validatedResult ();
       // log the data we found
       console.log(data);
       // tell the user we're adding a Input with an alert window
-      alert("Adding Input...");
+      alert(resultValue + "Answer!");
     });
 
 
